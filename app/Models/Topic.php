051a918d80230c21ecefd70e903b2131177158ce
lotus <3550087240@qq.com>
2024-07-14 11:01:13 +0800

    回复数据

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -51,4 +51,8 @@ class Topic extends Model
     {
         return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
