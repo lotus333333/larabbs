c6122dfce6e8f32dd65e359095f92097858dd252
lotus <3550087240@qq.com>
2024-07-16 15:33:04 +0800

    侧边栏链接

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,8 +31,10 @@ class AppServiceProvider extends ServiceProvider
     {
         //分页样式
         \Illuminate\Pagination\Paginator::useBootstrap();
-        Topic::observe(TopicObserver::class);
-        Reply::observe(ReplyObserver::class);
+        \App\Models\User::observe(\App\Observers\UserObserver::class);
+        \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+        \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
     }
 
 }
